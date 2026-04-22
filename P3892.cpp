66b854913bfbf7cf34ff1cb4b37b22a813b297c4
Golden-Gread <778966179@qq.com>
2026-04-22 16:44:31 +0800

    Add files via upload

---
+++ P3892.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+struct jr{
+	int cid,pid,uid,sid;
+	string res;
+	jr(int c,int p,int u,int s,string r):cid(c),pid(p),uid(u),sid(s),res(r){}
+	jr():cid(0),pid(0),uid(0),sid(0),res(""){}
+};
+
+vector<int,vector<int>>
+
+int main(){
+	return 0;
+}
