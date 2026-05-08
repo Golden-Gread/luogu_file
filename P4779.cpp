5482a40bba55210ee29c8064d77be8d971eb6a3e
Golden-Gread <778966179@qq.com>
2026-05-08 11:29:36 +0800

    Add files via upload

--- P4779.cpp
+++ P4779.cpp
@@ -3,34 +3,41 @@ using namespace std;
 
 const int INF=1e9;
 
+typedef pair<int,int> p;
+
 int main(){
     int n,m,s;
     cin>>n>>m>>s;
     
-    vector<vector<int>> graph(n+1,vector<int>(n+1,INF));
+    map<p,long long> graph;
+        
+
     for(int i=1;i<=n;i++)
-        graph[i][i]=0;
+        graph[p(i,i)]=0;
     for(int i=1;i<=m;i++){
         int u,v,w;
         cin>>u>>v>>w;
-        graph[u][v]=w;
-        graph[v][u]=w;
+        graph[p(u,v)]=w;
+        graph[p(v,u)]=w;
     }
 
     for (int k = 1; k <= n; ++k) {          
         for (int i = 1; i <= n; ++i) {      
             for (int j = 1; j <= n; ++j) {  
-                if (graph[i][k] != INF && graph[k][j] != INF) {
-                    if (graph[i][k] + graph[k][j] < graph[i][j]) {
-                        graph[i][j] = graph[i][k] + graph[k][j];
+                if (graph.find(p(i,j)) != graph.end() && graph.find(p(i,k)) != graph.end() && graph.find(p(k,j)) != graph.end()) {
+                    if (graph[p(i,k)] + graph[p(k,j)] < graph[p(i,j)]) {
+                        graph[p(i,j)] = graph[p(i,k)] + graph[p(k,j)];
                     }
                 }
             }
         }
     }
 
+    
+
+    
     for(int i=1;i<=n;i++){
-        cout<<graph[s][i]<<" ";
+       cout<<graph[p(s,i)]<<" ";
     }
 
     return 0;
